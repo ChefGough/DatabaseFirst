2acf417055c71eee13e8273b586122710f651baf
Alexander Gough <gougha3@my.erau.edu>
2018-10-23 09:12:48 -0400

    DatabseFirst

---
+++ WebApplication1/Models/Metadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class StudentMetadata
+    {
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName;
+
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName;
+
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName;
+
+        [Display(Name = "Enrollment Date")]
+        public Nullable<System.DateTime> EnrollmentDate;
+    }
+    public class EnrollmentMetaData
+    {
+        [Range(0, 4)]
+        public Nullable<decimal> Grade;
+    }
+}
---
+++ WebApplication1/Models/PartialClasses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+
+    }
+
+    [MetadataType(typeof(EnrollmentMetaData))]
+    public partial class Enrollment
+    {
+
+    }
+}
